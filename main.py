72ecc2c5e2a7a3f1b32fbfe7781f373033918b43
Lukas Jurak <jur42282@spseol.cz>
2024-03-20 13:23:38 +0100

    added b&w, red filters

--- main.py
+++ main.py
@@ -19,3 +19,34 @@ def greenFilter(image):
     image.show()
     print()
 
+def blackAndWhite(image):
+    x = 0
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = image.getpixel((x,y))
+            prumer = int((r+g+b)/2.7)
+            image.putpixel((x,y), (r , b, r))
+            if prumer > 150:
+                image.putpixel((x,y), (255, 255, 255))
+            else:
+                image.putpixel((x,y), (10, 8, 0))
+            y += 1
+        x += 1
+    image.show()
+
+def redFilter(image):
+    x = 0
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = image.getpixel((x,y))
+            prumer = int((r+g+b)/2.7)
+            image.putpixel((x,y), (r , b, r))
+            if prumer > 150:
+                image.putpixel((x,y), (255, 255, 255))
+            else:
+                image.putpixel((x,y), (200, 8, 0))
+            y += 1
+        x += 1
+    image.show()
