8c7834831d2b4cfca8d24395a544c2824cc8dfbc
Lukas Jurak <jur42282@spseol.cz>
2024-03-20 13:18:39 +0100

    green filter

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+image = Image.open("káves.png")
+sirka, vyska = image.size
+
+def greenFilter(image):
+    x  = 0
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = image.getpixel((x,y))
+            prumer = int((r+g+b)/2.4)
+            image.putpixel((x,y), (r , b, r))
+            if prumer > 150:
+                image.putpixel((x,y), (255, 255, 255))
+            else:
+                image.putpixel((x,y), (100, 130, 90))
+            y += 1
+        x += 1
+    image.show()
+    print()
+
