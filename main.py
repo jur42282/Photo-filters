e28c0b290c884d6e6d1038cb95d205de43fa5672
jur42282 <jur42282@spseol.cz>
2024-04-02 19:42:29 +0200

    main python file

--- main.py
+++ main.py
@@ -1,67 +1,32 @@
 from PIL import Image
+import filters
 
-def greenFilter(image):
-    x  = 0
-    while x < sirka:
-        y = 0
-        while y < vyska:
-            r, g, b = image.getpixel((x,y))
-            prumer = int((r+g+b)/2.4)
-            image.putpixel((x,y), (r , b, r))
-            if prumer > 150:
-                image.putpixel((x,y), (255, 255, 255))
-            else:
-                image.putpixel((x,y), (100, 130, 90))
-            y += 1
-        x += 1
-    image.show()
-    print()
+while True:
+    img = input("Enter the name of the picture: ")
+    try:
+        image = Image.open(img)
+        print("Image loaded successfully.")
+        sirka, vyska = image.size
+        break
+    except FileNotFoundError:
+        print("File not found. Please try again. Make sure you're entering the correct name of the picture and the file type.")
 
-def blackAndWhite(image):
-    x = 0
-    while x < sirka:
-        y = 0
-        while y < vyska:
-            r, g, b = image.getpixel((x,y))
-            prumer = int((r+g+b)/2.7)
-            image.putpixel((x,y), (r , b, r))
-            if prumer > 150:
-                image.putpixel((x,y), (255, 255, 255))
-            else:
-                image.putpixel((x,y), (10, 8, 0))
-            y += 1
-        x += 1
-    image.show()
-
-def redFilter(image):
-    x = 0
-    while x < sirka:
-        y = 0
-        while y < vyska:
-            r, g, b = image.getpixel((x,y))
-            prumer = int((r+g+b)/2.7)
-            image.putpixel((x,y), (r , b, r))
-            if prumer > 150:
-                image.putpixel((x,y), (255, 255, 255))
-            else:
-                image.putpixel((x,y), (200, 8, 0))
-            y += 1
-        x += 1
-    image.show()
-
-image = Image.open("kaves.png")
-sirka, vyska = image.size
-print("What filter would you like to apply to your image? (1, 2, 3)")
-print("1. Green Filter")
-print("2. Black and White Filter")
-print("3. Red Filter")
-uInput = input()
-if uInput == "1":
-    greenFilter(image)
-elif uInput == "2":
-    blackAndWhite(image)
-elif uInput == "3":
-    redFilter(image)
-else:
-    print("Invalid input. Please try again.")
+while True:
+    print("What filter would you like to apply to your image? (1, 2, 3)")
+    print("1. Green Filter")
+    print("2. Black and White Filter")
+    print("3. Red Filter")
     uInput = input()
+    if uInput == "1":
+        filters.greenFilter(image, sirka, vyska)
+        break
+    elif uInput == "2":
+        filters.blackAndWhite(image, sirka, vyska)
+        break
+    elif uInput == "3":
+        filters.redFilter(image, sirka, vyska)
+        break
+    else:
+        print("Invalid input. Please try again.")
+        continue
+print("Done!")
